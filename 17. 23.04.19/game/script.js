b11fd0a145d7a50718d2cdd4f9c0c360a83ee691
Leonid <leonxaoss@gmail.com>
2019-05-05 22:04:39 +0300

    fix_17_18

--- 17. 23.04.19/game/script.js
+++ 17. 23.04.19/game/script.js
@@ -14,20 +14,32 @@ const coorX = Math.floor(Math.random() * ($map.offsetWidth - 2)+ 2);
 const coorY = Math.floor(Math.random() * ($map.offsetHeight - 2)+ 2);
 
 $map.addEventListener('click', function (event) {
-    let a = coorX - event.offsetX;
-    let b = coorY - event.offsetY;
-    let res = Math.sqrt(a * a + b * b);
+    const a = coorX - event.offsetX;
+    const b = coorY - event.offsetY;
+    const res = Math.sqrt(a * a + b * b);
 
-    switch (true) {
-        case res < 30: alert('Ура клад найден');
-        break;
-        case res < 100 : alert('Горячо');
-        break;
-        case res < 170 : alert('Тепло');
-        break;
-        case res < 220 : alert('Холодно');
-        break;
-        case res > 220 : alert('Очень холодно');
-        break;
+    if(res < 30){
+        alert('Ура клад найден');
+    } else if(res < 100){
+        alert('Горячо');
+    } else if(res < 170){
+        alert('Тепло');
+    } else if(res < 220){
+        alert('Холодно');
+    } else {
+        alert('Очень Холодно');
     }
+
+    // switch (true) {
+    //     case res < 30: alert('Ура клад найден');
+    //         break;
+    //     case res < 100 : alert('Горячо');
+    //         break;
+    //     case res < 170 : alert('Тепло');
+    //         break;
+    //     case res < 220 : alert('Холодно');
+    //         break;
+    //     case res > 220 : alert('Очень холодно');
+    //         break;
+    // }
 });
